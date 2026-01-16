c9e0219f75781ed07993815df3cf19ddef37f285
Suhur <zuhurabdi4108@gmail.com>
2026-01-16 13:11:34 +0300

    Intial commit: introduction

---
+++ intro.js
@@ -0,0 +1,6 @@
+const name = "Suhur Abdirahman";
+const age = 25;
+const favoriteProgamming = "React";
+console.log("My Name is:" + name);
+console.log("My Age is:" + age);
+console.log("My Favorite Programming Language is:" + favoriteProgamming);
